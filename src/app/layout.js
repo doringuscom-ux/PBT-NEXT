6fb8643a51f8b1c4c29c28ef4ca6a49c5c80df51
doringuscom-ux <doringus.com@gmail.com>
2026-07-16 16:12:39 +0530

    feat: initialize project styles with Tailwind and configure root layout with fonts and analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,11 @@
 import "./globals.css";
 import Providers from "../components/Providers";
+import Script from "next/script";
+import { Montserrat, Open_Sans, Roboto } from 'next/font/google';
+
+const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '700', '900'], variable: '--font-headline', display: 'swap' });
+const openSans = Open_Sans({ subsets: ['latin'], weight: ['400', '600', '700'], variable: '--font-sans', display: 'swap' });
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700', '900'], variable: '--font-menu', display: 'swap' });
 
 export const metadata = {
   title: "PB TADKA - Film News",
@@ -15,27 +21,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={`${montserrat.variable} ${openSans.variable} ${roboto.variable}`}>
       <head>
         <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
+        <link 
+            rel="stylesheet" 
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+            media="print" 
+            onLoad="this.media='all'" 
+        />
+        <noscript>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        </noscript>
         
         {/* Google Adsense */}
         <meta name="google-adsense-account" content="ca-pub-6214614018313479" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6214614018313479" crossOrigin="anonymous"></script>
-        
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QVGGSCFLGE"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-QVGGSCFLGE');
-            `,
-          }}
-        />
         
         {/* Auto-Reload on Chunk Error */}
         <script
@@ -74,6 +75,27 @@ export default function RootLayout({ children }) {
         <Providers>
           {children}
         </Providers>
+
+        <Script 
+            id="adsense"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6214614018313479" 
+            strategy="lazyOnload"
+            crossOrigin="anonymous" 
+        />
+
+        <Script 
+            id="ga-script"
+            src="https://www.googletagmanager.com/gtag/js?id=G-QVGGSCFLGE" 
+            strategy="lazyOnload" 
+        />
+        <Script id="ga-config" strategy="lazyOnload">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QVGGSCFLGE');
+            `}
+        </Script>
       </body>
     </html>
   );
