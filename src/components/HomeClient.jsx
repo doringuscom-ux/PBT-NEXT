ac34efd53833ec06277f974a6da2120d718c1bcb
doringuscom-ux <doringus.com@gmail.com>
2026-08-01 16:06:10 +0530

    feat: create HomeClient component with hero, sidebar, and modular content sections

--- src/components/HomeClient.jsx
+++ src/components/HomeClient.jsx
@@ -57,7 +57,7 @@ export default function HomeClient() {
             <CompactCelebSlider />
             <NewsGrid />
             <CompactSongsSlider />
-            <CelebGrid />
+            {/* <CelebGrid /> */}
           </div>
 
 
